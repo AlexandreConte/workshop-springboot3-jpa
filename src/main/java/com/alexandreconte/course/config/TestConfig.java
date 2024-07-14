81649972f9d8b5cceaaccca7e6b1bf6a6a1dffce
Alexandre Conte <alexandreconte.dev@gmail.com>
2024-07-14 14:16:21 -0300

    OrderStatus enum

--- src/main/java/com/alexandreconte/course/config/TestConfig.java
+++ src/main/java/com/alexandreconte/course/config/TestConfig.java
@@ -2,8 +2,10 @@ package com.alexandreconte.course.config;
 
 import com.alexandreconte.course.entities.Order;
 import com.alexandreconte.course.entities.User;
+import com.alexandreconte.course.entities.enums.OrderStatus;
 import com.alexandreconte.course.repositories.OrderRepository;
 import com.alexandreconte.course.repositories.UserRepository;
+import org.aspectj.weaver.ast.Or;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
@@ -27,9 +29,9 @@ public class TestConfig implements CommandLineRunner {
         User user1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
         User user2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
 
-        Order order1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), user1);
-        Order order2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), user2);
-        Order order3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), user1);
+        Order order1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), user1, OrderStatus.PAID);
+        Order order2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), user2, OrderStatus.WAITING_PAYMENT);
+        Order order3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), user1, OrderStatus.WAITING_PAYMENT);
 
         userRepository.saveAll(Arrays.asList(user1, user2));
         orderRepository.saveAll(Arrays.asList(order1, order2, order3));
