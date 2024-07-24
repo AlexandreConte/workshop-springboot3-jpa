ec02b9381e11e0829fec3e9b14849fe5dd9fce80
Alexandre Conte <alexandreconte.dev@gmail.com>
2024-07-24 11:15:34 -0300

    OrderItem, many-to-many association with extra attributes PART 1

---
+++ src/main/java/com/alexandreconte/course/entities/OrderItem.java
@@ -0,0 +1,75 @@
+package com.alexandreconte.course.entities;
+
+import com.alexandreconte.course.entities.pk.OrderItemPk;
+import jakarta.persistence.EmbeddedId;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+@Entity
+@Table(name = "tb_order_item")
+public class OrderItem implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @EmbeddedId
+    private OrderItemPk id;
+
+    private Integer quantity;
+    private Integer price;
+
+    public OrderItem() { }
+
+    public OrderItem(Order order, Product product, Integer price, Integer quantity) {
+        super();
+        id.setOrder(order);
+        id.setProduct(product);
+        this.price = price;
+        this.quantity = quantity;
+    }
+
+    public Order getOrder() {
+        return id.getOrder();
+    }
+
+    public void setOrder(Order order) {
+        id.setOrder(order);
+    }
+
+    public Product getProduct() {
+        return id.getProduct();
+    }
+
+    public void setProduct(Product product) {
+        id.setProduct(product);
+    }
+
+    public Integer getPrice() {
+        return price;
+    }
+
+    public void setPrice(Integer price) {
+        this.price = price;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof OrderItem orderItem)) return false;
+        return Objects.equals(id, orderItem.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+}
